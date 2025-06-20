e0827015c18bc884a343dd63fa9cc37fb3ff038d
Alaukik Miishra <151136125+Alaukik1210@users.noreply.github.com>
2025-06-20 20:40:57 +0530

    bypassed a errorr

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
    reactStrictMode: true,
-   rules: {
-  '@typescript-eslint/ban-ts-comment': 'off',
-},
+   
 
 };
 
